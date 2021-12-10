282b8d627127530740ab35cd4e9794ec9215dde2
James Stringer <38541878+jamesstringerparsec@users.noreply.github.com>
2021-12-10 12:27:52 +0100

    Don't use insider-preview versions of Hyper-V

--- CopyFilesToVM.ps1
+++ CopyFilesToVM.ps1
@@ -4301,7 +4301,7 @@ param(
 [string]$autologon
 )
     Modify-AutoUnattend -username "$username" -password "$password" -autologon $autologon -hostname $VMName -UnattendPath $UnattendPath
-    $MaxAvailableVersion = (Get-VMHostSupportedVersion).Version | Select-Object -Last 1
+    $MaxAvailableVersion = (Get-VMHostSupportedVersion).Version | Where-Object {$_.Major -lt 254}| Select-Object -Last 1 
     Convert-WindowsImage -SourcePath $SourcePath -Edition $Edition -VHDFormat $Vhdformat -VHDPath $VhdPath -DiskLayout $DiskLayout -UnattendPath $UnattendPath -GPUName $GPUName -Team_ID $Team_ID -Key $Key -SizeBytes $SizeBytes| Out-Null
     New-VM -Name $VMName -MemoryStartupBytes $MemoryAmount -VHDPath $VhdPath -Generation 2 -SwitchName "Default Switch" -Version $MaxAvailableVersion | Out-Null
     Set-VM -Name $VMName -ProcessorCount $CPUCores -CheckpointType Disabled -LowMemoryMappedIoSpace 3GB -HighMemoryMappedIoSpace 32GB -GuestControlledCacheTypes $true -AutomaticStopAction ShutDown
