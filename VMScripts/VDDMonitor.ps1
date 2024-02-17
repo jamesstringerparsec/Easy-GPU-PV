a25d49e290b25bf496364a69796cf0b53427d647
James Stringer <38541878+jamesstringerparsec@users.noreply.github.com>
2024-02-16 23:03:56 -0800

    Update VDDMonitor.ps1

--- VMScripts/VDDMonitor.ps1
+++ VMScripts/VDDMonitor.ps1
@@ -1,4 +1,4 @@
-﻿$Global:VDD
+$Global:VDD
 
 Function GetVDDState {
 $Global:VDD = Get-PnpDevice | where {$_.friendlyname -like "Parsec Virtual Display Adapter"}
@@ -7,8 +7,12 @@ $Global:VDD = Get-PnpDevice | where {$_.friendlyname -like "Parsec Virtual Displ
 While (1 -gt 0) {
     GetVDDSTate
     If ($Global:VDD -eq $NULL){
-    Exit
-    }
+        Start-Sleep -s 10
+        }
+    If (!((Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WUDF\Services\ParsecVDA\Parameters\').PSObject.Properties.Name -contains "PreferredRenderAdapterVendorId")) {
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WUDF\Services\ParsecVDA\Parameters\' -Name PreferredRenderAdapterVendorId -PropertyType DWORD -Value 5140 | Out-Null
+        Disable-PnpDevice -InstanceId $Global:VDD.InstanceId -Confirm:$false
+        }
     Do {
         Enable-PnpDevice -InstanceId $Global:VDD.InstanceId -Confirm:$false
         Start-Sleep -s 5
